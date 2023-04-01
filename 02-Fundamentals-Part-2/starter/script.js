e5bcfd0e906237b7012e29df04401f984b1ec2cc
Sashen Moodley <sashenmoodley321@gmail.com>
2023-04-01 18:00:04 +0200

    added coding challenge 3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -24,10 +24,44 @@
 ///////////////////////////////////////
 // Coding Challenge #2
 
-const calcTip = billTotal => (billTotal >= 50 && billTotal <= 300) ? 0.15*billTotal: 0.2*billTotal;
+// const calcTip = billTotal => (billTotal >= 50 && billTotal <= 300) ? 0.15*billTotal: 0.2*billTotal;
+//
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+//
+// console.log(bills, tips, total)
 
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+///////////////////////////////////////
+// Coding Challenge #3
 
-console.log(bills, tips, total)
+// const mark = {
+//     fullName: 'Mark Miller',
+//     mass: 79,
+//     height: 1.69,
+//     calcBMI: function() {
+//         this.bmi = (this.mass/this.height**2);
+//         return this.bmi;
+//     }
+// }
+//
+// const john = {
+//     fullName: 'John Smith',
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function() {
+//         this.bmi = (this.mass/this.height**2);
+//         return this.bmi;
+//     }
+// }
+//
+// const markBMI = mark.calcBMI();
+// const johnBMI = john.calcBMI();
+//
+// if (markBMI > johnBMI) {
+//     console.log(`${mark.fullName}'s BMI (${markBMI}) is higher than ${john.fullName}'s BMI (${johnBMI})`)
+// } else if (johnBMI > markBMI) {
+//     console.log(`${john.fullName}'s BMI (${johnBMI}) is higher than ${mark.fullName}'s BMI (${markBMI})`)
+// } else {
+//     console.log(`${mark.fullName} and ${john.fullName} have the same BMI (${markBMI})`)
+// }
