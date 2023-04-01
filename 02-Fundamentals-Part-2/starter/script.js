baa8f7d3ecad86cebccafd24e71580f19359a8a5
Sashen Moodley <71750671+MagneticPanda@users.noreply.github.com>
2023-04-01 17:06:14 +0200

    added coding challenge 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -22,3 +22,12 @@
 // }
 
 ///////////////////////////////////////
+// Coding Challenge #2
+
+const calcTip = billTotal => (billTotal >= 50 && billTotal <= 300) ? 0.15*billTotal: 0.2*billTotal;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, total)
