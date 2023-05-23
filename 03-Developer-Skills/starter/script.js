7c78924789710d10fe3eb21e206b58033bdb7bde
Sashen Moodley <sashenmoodley321@gmail.com>
2023-05-23 19:53:17 +0200

    updated developer skills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const printForecast = function (arr) {
+    let displayString = '... '
+    for (let i = 0; i < arr.length; i++) {
+        displayString += `${arr[i]}ºC in ${i} days ... `
+    }
+    console.log(displayString);
+}
+
+let array1 = [17, 21, 23];
+let array2 = [12, 5, -5, 0, 4];
+
+printForecast(array1);
+printForecast(array2);
